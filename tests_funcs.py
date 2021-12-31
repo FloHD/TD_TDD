471b198c8c72fc44f1b56f04330e6dd4beb875c5
FloHD <f.hameletdelval@gmail.com>
2021-12-31 10:26:35 +0100

    ajout tests fonction de moyennage

--- tests_funcs.py
+++ tests_funcs.py
@@ -9,5 +9,12 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.min_int(-1,2),-1)
         self.assertEqual(funcs.min_int(0,0),0)
 
+    def test_mean(self):
+        self.assertEqual(funcs.mean({2,3,4}),3)
+        self.assertEqual(funcs.mean({-2,-3,-4}),-3)
+        self.assertEqual(funcs.mean({-2,2,2,-2}),0)
+        self.assertEqual(funcs.mean({-1.2,3.6,4}),(-1.2 + 3.6 + 4) / 3)
+        self.assertEqual(funcs.mean({1}),1)
+
 if __name__ == '__main__':
     unittest.main()
