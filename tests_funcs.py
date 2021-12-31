bd95dc0151ecd37c84029582a987486d39700497
FloHD <f.hameletdelval@gmail.com>
2021-12-31 11:10:56 +0100

    ajout tests fonction médiane

--- tests_funcs.py
+++ tests_funcs.py
@@ -1,6 +1,7 @@
 import funcs
 import unittest
 
+
 class TestFuncs(unittest.TestCase):
 
     def test_min_int(self):
@@ -13,8 +14,19 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.mean({2,3,4}),3)
         self.assertEqual(funcs.mean({-2,-3,-4}),-3)
         self.assertEqual(funcs.mean({-2,2,2,-2}),0)
-        self.assertEqual(funcs.mean({-1.2,3.6,4}),(-1.2 + 3.6 + 4) / 3)
-        self.assertEqual(funcs.mean({1}),1)
+        self.assertEqual(funcs.mean({-1.2,3.6,4}),(-1.2 + 3.6 + 4) / 3.0)
+        self.assertEqual(funcs.mean({2}),2)
+        self.assertEqual(funcs.mean({}),None)
+
+    def test_median(self):
+        self.assertEqual(funcs.median({3,1,4}),3)
+        self.assertEqual(funcs.median({-2,-1,-4}),-2)
+        self.assertEqual(funcs.median({1,3,4,5}),3.5)
+        self.assertEqual(funcs.median({-1,-3,-7,-2}),-2.5)
+        self.assertEqual(funcs.median({-1.2,3.6,4}),3.6)
+        self.assertEqual(funcs.median({-1.2,3.6,4,2.6}),3.1)
+        self.assertEqual(funcs.median({2}),2)
+        self.assertEqual(funcs.median({}),None)
 
 if __name__ == '__main__':
     unittest.main()
