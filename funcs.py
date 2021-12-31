7da96cca7a6117b424af473fa59cc60a40e7a50c
FloHD <f.hameletdelval@gmail.com>
2021-12-31 11:11:40 +0100

    ajout fonction médiane

--- funcs.py
+++ funcs.py
@@ -5,5 +5,23 @@ def min_int(a, b):
 		return b
 
 def mean(data):
-	mean = sum(data)/len(data)
-	return mean
+	if len(data) == 0:
+		return None
+
+	else:
+		mean = sum(data)/len(data)
+		return mean
+
+
+def median(data):
+	data = sorted(data)
+	l = len(data)
+	
+	if l < 1:
+		return None
+
+	if l % 2 == 0:
+		return (data[(l-1)/2] + data[(l+1)/2])/2.0
+
+	else:
+		return data[(l-1)/2]
